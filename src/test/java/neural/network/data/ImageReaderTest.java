d89245678698b6e207a18ed1dc6e58cce550fa6a
HCSO\HEFE002 <henry.fender@hennepin.us>
2017-12-05 14:09:39 -0600

    Fix test

--- src/test/java/neural/network/data/ImageReaderTest.java
+++ src/test/java/neural/network/data/ImageReaderTest.java
@@ -11,8 +11,8 @@ public class ImageReaderTest {
 	
 	@Test
 	public void testImageRead() throws IOException {
+		System.out.println("Testing Image Reader on mnist_test.csv...\n");
 		List<Image> dataset = ImageReader.read("/mnist_test.csv");
-		System.out.println(dataset.size());
 		assertEquals(76, dataset.get(47).getData()[65]);
 		assertEquals(174, dataset.get(47).getData()[66]);
 		assertEquals(2, dataset.get(66).getData()[67]);
